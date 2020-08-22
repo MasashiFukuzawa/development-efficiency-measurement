04ca672022d3b41e44038c21528c4a30bde158b8
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-22 22:47:35 +0900

    feat: implement user model

--- src/domain/models/user/user.ts
+++ src/domain/models/user/user.ts
@@ -1,1 +1,39 @@
-export class User {}
+import { UserId } from './value_objects/user_id';
+import { UserName } from './value_objects/user_name';
+
+export class User {
+  private readonly id: UserId;
+  private readonly name: UserName;
+  private readonly createdAt: Date;
+  constructor(id: string, name: string, createdAt = new Date()) {
+    this.id = new UserId(id);
+    this.name = new UserName(name);
+    this.createdAt = createdAt;
+  }
+
+  getId(): UserId {
+    return this.id;
+  }
+
+  getName(): UserName {
+    return this.name;
+  }
+
+  getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  getRecordNotUniqueErrorMessage(): string {
+    return `Unique制約に引っ掛かりました。${this.getName().toString()} さんは既に登録されています`;
+  }
+
+  static validate(id: string, name: string): string | null {
+    try {
+      new UserId(id);
+      new UserName(name);
+      return null;
+    } catch (e) {
+      return e;
+    }
+  }
+}
--- src/domain/models/user/value_objects/user_name.ts
+++ src/domain/models/user/value_objects/user_name.ts
@@ -1,1 +1,14 @@
-export class UserName {}
+export class UserName {
+  name: string;
+  constructor(name: string) {
+    if (!name) throw new Error('UserNameが存在しません');
+    if (typeof name !== 'string') {
+      throw new Error('UserNameはstring型でなければなりません');
+    }
+    this.name = name;
+  }
+
+  toString(): string {
+    return this.name;
+  }
+}
