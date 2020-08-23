72c979d9e6b0008ef4c0af3b1cd2d07beb832a43
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-23 20:54:23 +0900

    feat: implement measurement start interactor

--- src/domain/applications/measurement/measurement_start_interactor.ts
+++ src/domain/applications/measurement/measurement_start_interactor.ts
@@ -1,4 +1,37 @@
+import { MeasurementStartOutputData } from '../../../use_case/measurement/start/measurement_start_output_data';
 import { MeasurementStartUseCaseInterface } from '../../../use_case/measurement/start/measurement_start_use_case_interface';
+import { ReplyPresenter } from '../../../webhook_app/common/presenters/reply/reply_presenter';
+import { Measurement } from '../../models/measurement/measurement';
+import { MeasurementRepositoryInterface } from '../../models/measurement/measurement_repository_interface';
+import { UserRepositoryInterface } from '../../models/user/user_repository_interface';
+import TextOutput = GoogleAppsScript.Content.TextOutput;
 
 export class MeasurementStartInteractor
-  implements MeasurementStartUseCaseInterface {}
+  implements MeasurementStartUseCaseInterface {
+  constructor(
+    private readonly userRepository: UserRepositoryInterface,
+    private readonly measurementRepository: MeasurementRepositoryInterface,
+    private readonly replyPresenter: ReplyPresenter,
+  ) {}
+
+  handle(userId: string, userName: string, description?: string): TextOutput {
+    const outputData = new MeasurementStartOutputData();
+
+    const user = this.userRepository.findByUserId(userId);
+    if (!user) {
+      const errorMessage = outputData.getUserNotFoundErrorMessage(userName);
+      return this.replyPresenter.reply(errorMessage);
+    }
+
+    const lastMeasurement = this.measurementRepository.last(userId);
+    if (Measurement.isConflicting(lastMeasurement)) {
+      const errorMessage = outputData.getConflictErrorMessage();
+      return this.replyPresenter.reply(errorMessage);
+    }
+
+    this.measurementRepository.stampStartAt(userId, description);
+
+    const startMessage = outputData.getStartMessage(userName, description);
+    return this.replyPresenter.reply(startMessage);
+  }
+}
--- src/domain/models/measurement/measurement_repository_interface.ts
+++ src/domain/models/measurement/measurement_repository_interface.ts
@@ -1,1 +1,6 @@
-export interface MeasurementRepositoryInterface {}
+import { Measurement } from './measurement';
+
+export interface MeasurementRepositoryInterface {
+  last(userId: string): Measurement | null;
+  stampStartAt(userId: string, description?: string): Measurement;
+}
