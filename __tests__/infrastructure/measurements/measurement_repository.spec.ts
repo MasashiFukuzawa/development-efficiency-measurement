6e87f2569d6efc3eb919b3d3209bc19aae45f3eb
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-23 22:38:09 +0900

    test: add measurement repository test

---
+++ __tests__/infrastructure/measurements/measurement_repository.spec.ts
@@ -0,0 +1,90 @@
+import { MeasurementRepository } from '../../../src/infrastructure/measurements/measurement_repository';
+
+describe('MeasurementRepository', () => {
+  SpreadsheetApp.openById = jest.fn(() => ({
+    getSheetByName: jest.fn(() => ({
+      getLastRow: jest.fn(() => 5),
+      getLastColumn: jest.fn(() => 4),
+      getRange: jest.fn(() => ({
+        getValues: jest.fn(() => [
+          [
+            'IM1234',
+            new Date(2020, 1, 1, 10, 0, 0, 0),
+            new Date(2020, 1, 1, 11, 0, 0, 0),
+            undefined,
+          ],
+          [
+            'KB5678',
+            new Date(2020, 2, 1, 10, 0, 0, 0),
+            new Date(2020, 2, 1, 11, 0, 0, 0),
+            undefined,
+          ],
+          [
+            'IM1234',
+            new Date(2020, 3, 1, 10, 0, 0, 0),
+            new Date(2020, 3, 1, 11, 0, 0, 0),
+            undefined,
+          ],
+          [
+            'ST5678',
+            new Date(2020, 4, 1, 10, 0, 0, 0),
+            new Date(2020, 4, 1, 11, 0, 0, 0),
+            undefined,
+          ],
+          [
+            'IM1234',
+            new Date(2020, 5, 1, 10, 0, 0, 0),
+            new Date(2020, 5, 1, 11, 0, 0, 0),
+            undefined,
+          ],
+        ]),
+        setValues: jest.fn(),
+      })),
+    })),
+  })) as any;
+
+  PropertiesService.getScriptProperties = jest.fn(() => ({
+    getProperty: jest.fn(() => 'SPREAD_SHEET_ID'),
+  })) as any;
+
+  const measurementRepository = new MeasurementRepository();
+
+  describe('#last', () => {
+    describe('when data exist', () => {
+      it('returns a measurement resource', () => {
+        const lastMeasurement = measurementRepository.last('IM1234');
+        const userId = lastMeasurement.getUserId().toString();
+        const startAt = lastMeasurement.getMeasurementStartAt().toDate();
+        const stopAt = lastMeasurement.getMeasurementStopAt().toDate();
+        const description = lastMeasurement.getDescription().toString();
+        expect(userId).toBe('IM1234');
+        expect(startAt).toStrictEqual(new Date(2020, 5, 1, 10, 0, 0, 0));
+        expect(stopAt).toStrictEqual(new Date(2020, 5, 1, 11, 0, 0, 0));
+        expect(description).toBe(undefined);
+      });
+    });
+
+    describe('when data do not exist', () => {
+      it('returns null', () => {
+        const measurement = measurementRepository.last('MY1234');
+        expect(measurement).toBe(null);
+      });
+    });
+  });
+
+  describe('#stampStartAt', () => {
+    it('returns measurement resource', () => {
+      jest.spyOn(global, 'Date').mockImplementation();
+
+      const measurement = measurementRepository.stampStartAt('IM1234');
+      const userId = measurement.getUserId().toString();
+      const startAt = measurement.getMeasurementStartAt().toDate();
+      const stopAt = measurement.getMeasurementStopAt()?.toDate();
+      const description = measurement.getDescription()?.toString();
+      expect(userId).toBe('IM1234');
+      expect(startAt).toStrictEqual(new Date());
+      expect(stopAt).toBe(undefined);
+      expect(description).toBe(undefined);
+    });
+  });
+});
