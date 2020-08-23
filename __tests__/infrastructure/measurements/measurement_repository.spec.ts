21de4226ef65cc4cd527fcd7a2fee660f5133ef5
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-24 00:23:20 +0900

    test: add measurement repository test

--- __tests__/infrastructure/measurements/measurement_repository.spec.ts
+++ __tests__/infrastructure/measurements/measurement_repository.spec.ts
@@ -1,3 +1,4 @@
+import { Measurement } from '../../../src/domain/models/measurement/measurement';
 import { MeasurementRepository } from '../../../src/infrastructure/measurements/measurement_repository';
 
 describe('MeasurementRepository', () => {
@@ -83,8 +84,31 @@ describe('MeasurementRepository', () => {
       const description = measurement.getDescription()?.toString();
       expect(userId).toBe('IM1234');
       expect(startAt).toStrictEqual(new Date());
-      expect(stopAt).toBe(undefined);
-      expect(description).toBe(undefined);
+      expect(typeof stopAt).toBe('undefined');
+      expect(typeof description).toBe('undefined');
+    });
+  });
+
+  describe('#stampStopAt', () => {
+    it('returns measurement resource', () => {
+      const lastMeasurement = new Measurement(
+        'IM1234',
+        new Date(2020, 5, 1, 10, 0, 0, 0),
+        new Date(2020, 5, 1, 11, 0, 0, 0),
+        undefined,
+      );
+      const measurement = measurementRepository.stampStopAt(
+        'IM1234',
+        lastMeasurement,
+      );
+      const userId = measurement.getUserId().toString();
+      const startAt = measurement.getMeasurementStartAt().toDate();
+      const stopAt = measurement.getMeasurementStopAt().toDate();
+      const description = measurement.getDescription()?.toString();
+      expect(userId).toBe('IM1234');
+      expect(startAt).toStrictEqual(new Date(2020, 5, 1, 10, 0, 0, 0));
+      expect(stopAt).toStrictEqual(new Date(2020, 5, 1, 11, 0, 0, 0));
+      expect(typeof description).toBe('undefined');
     });
   });
 });
