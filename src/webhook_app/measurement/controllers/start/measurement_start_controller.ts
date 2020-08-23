9d57a3a6da3a412b39e3fc3b24929ad23657e1df
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-23 22:57:23 +0900

    feat: implement from doPost to controller

--- src/webhook_app/measurement/controllers/start/measurement_start_controller.ts
+++ src/webhook_app/measurement/controllers/start/measurement_start_controller.ts
@@ -1,1 +1,12 @@
-export class MeasurementStartController {}
+import { MeasurementStartUseCaseInterface } from '../../../../use_case/measurement/start/measurement_start_use_case_interface';
+import TextOutput = GoogleAppsScript.Content.TextOutput;
+
+export class MeasurementStartController {
+  constructor(
+    private readonly measurementStartUseCase: MeasurementStartUseCaseInterface,
+  ) {}
+
+  start(userId: string, userName: string, description?: string): TextOutput {
+    return this.measurementStartUseCase.handle(userId, userName, description);
+  }
+}
