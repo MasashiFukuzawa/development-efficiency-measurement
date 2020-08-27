4b981df7d0230cdcb0cc45d2987f153c509961c8
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-27 09:46:58 +0900

    refactor: remove unnecessary import

--- src/tasks/theoretical_time/controllers/calculate/theoretical_time_calculate_controller.ts
+++ src/tasks/theoretical_time/controllers/calculate/theoretical_time_calculate_controller.ts
@@ -1,4 +1,3 @@
-import { TheoreticalTimeCalculateInputData } from '../../../../use_case/theoretical_time/calculate/theoretical_time_calculate_input_data';
 import { TheoreticalTimeCalculateUseCaseInterface } from '../../../../use_case/theoretical_time/calculate/theoretical_time_calculate_use_case_interface';
 
 export class TheoreticalTimeCalculateController {
