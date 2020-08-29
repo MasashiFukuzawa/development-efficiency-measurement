178cd50cf77ddaa945fb4b763eeb930aa68a6f43
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-29 21:54:04 +0900

    feat: implement from gas function to controller

--- src/webhook_app/summary_report/controllers/notify/summary_report_notify_controller.ts
+++ src/webhook_app/summary_report/controllers/notify/summary_report_notify_controller.ts
@@ -1,1 +1,11 @@
-export class SummaryReportNotifyController {}
+import { SummaryReportNotifyUseCaseInterface } from '../../../../use_case/summary_report/notify/summary_report_notify_use_case_interface';
+
+export class SummaryReportNotifyController {
+  constructor(
+    private readonly summaryReportNotifyUseCase: SummaryReportNotifyUseCaseInterface,
+  ) {}
+
+  notify(): void {
+    this.summaryReportNotifyUseCase.handle();
+  }
+}
