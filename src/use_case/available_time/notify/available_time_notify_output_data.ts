08bd232177b56e09756926f0b5491c5be74a9290
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-08-30 11:14:08 +0900

    fix: Math round decimal

--- src/use_case/available_time/notify/available_time_notify_output_data.ts
+++ src/use_case/available_time/notify/available_time_notify_output_data.ts
@@ -1,6 +1,8 @@
 export class AvailableTimeNotifyOutputData {
   getMessage(userId: string, todaysAvailableTime: number): string {
     return `<@${userId}>
-本日の実装可能時間は ${todaysAvailableTime / (60 * 60 * 1000)}h です。`;
+本日の実装可能時間は ${
+      Math.round((todaysAvailableTime / (60 * 60 * 1000)) * 100) / 100
+    }h です。`;
   }
 }
--- src/use_case/summary_report/notify/summary_report_notify_output_data.ts
+++ src/use_case/summary_report/notify/summary_report_notify_output_data.ts
@@ -11,22 +11,27 @@ export class SummaryReportNotifyOutputData {
 今週もお疲れ様でした！今週の開発効率の計測結果をお知らせします！
 
 ======================================================
-開発効率指数: ${Math.round(summaryReport.getKpiValue().toNumber())}
+開発効率指数: ${Math.round(summaryReport.getKpiValue().toNumber() * 100) / 100}
 
 
     内訳:
-        実装時間合計: ${Math.round(
-          summaryReport.getTotalImplementHour().toNumber(),
-        )}h
+        実装時間合計: ${
+          Math.round(summaryReport.getTotalImplementHour().toNumber() * 100) /
+          100
+        }h
         計測回数: ${summaryReport.getMeasurementCount().toNumber()}回
-        平均実装時間: ${Math.round(
-          summaryReport.getAverageImplementHour().toNumber(),
-        )}h
-        カレンダー上での空き時間合計: ${Math.round(
-          summaryReport.getTheoreticalAvailableHour().toNumber(),
-        )}h
+        平均実装時間: ${
+          Math.round(summaryReport.getAverageImplementHour().toNumber() * 100) /
+          100
+        }h
+        カレンダー上での空き時間合計: ${
+          Math.round(
+            summaryReport.getTheoreticalAvailableHour().toNumber() * 100,
+          ) / 100
+        }h
         差し込み率: ${
-          1 - Math.round(summaryReport.getAvailableRate().toNumber())
+          1 -
+          Math.round(summaryReport.getAvailableRate().toNumber() * 100) / 100
         }
 
 
@@ -36,18 +41,25 @@ export class SummaryReportNotifyOutputData {
 (参考) 今週のユーザー全体の平均値:
 
 
-    開発効率指標: ${Math.round(standardValue.getKpiValue().toNumber())}
-    実装時間合計: ${Math.round(
-      standardValue.getTotalImplementHour().toNumber(),
-    )}h
-    計測回数: ${Math.round(standardValue.getMeasurementCount().toNumber())}回
-    平均実装時間: ${Math.round(
-      standardValue.getAverageImplementHour().toNumber(),
-    )}h
-    カレンダー上での空き時間合計: ${Math.round(
-      standardValue.getTheoreticalAvailableHour().toNumber(),
-    )}h
-    差し込み率: ${1 - Math.round(standardValue.getAvailableRate().toNumber())}
+    開発効率指標: ${
+      Math.round(standardValue.getKpiValue().toNumber() * 100) / 100
+    }
+    実装時間合計: ${
+      Math.round(standardValue.getTotalImplementHour().toNumber() * 100) / 100
+    }h
+    計測回数: ${
+      Math.round(standardValue.getMeasurementCount().toNumber() * 100) / 100
+    }回
+    平均実装時間: ${
+      Math.round(standardValue.getAverageImplementHour().toNumber() * 100) / 100
+    }h
+    カレンダー上での空き時間合計: ${
+      Math.round(standardValue.getTheoreticalAvailableHour().toNumber() * 100) /
+      100
+    }h
+    差し込み率: ${
+      1 - Math.round(standardValue.getAvailableRate().toNumber() * 100) / 100
+    }
 ======================================================`;
   }
 }
