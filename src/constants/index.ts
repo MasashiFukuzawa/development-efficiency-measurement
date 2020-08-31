0adcb922260ddec5c892a18a9237d843b0d3967d
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-09-01 08:48:26 +0900

    fix: use github url in help message

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export class GlobalConstants {
   static readonly EMAIL_REGEXP = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+  static readonly GITHUB_URL =
+    'https://github.com/MasashiFukuzawa/development-efficiency-measurement/issues';
 }
