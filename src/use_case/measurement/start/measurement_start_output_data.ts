fc0d7861d9d3a6175fa1b9af95c984534308fd66
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-09-02 23:12:19 +0900

    fix: change command name

--- src/use_case/measurement/start/measurement_start_output_data.ts
+++ src/use_case/measurement/start/measurement_start_output_data.ts
@@ -5,13 +5,13 @@ export class MeasurementStartOutputData {
 
   getUserNotFoundErrorMessage(userName: string): string {
     return `${userName} さんのユーザーデータは存在しません。
-\`/kaihatsu create_user xxx@finc.com\` コマンドを実行し、ユーザー登録を行ってからご利用下さい。`;
+\`/measurement user xxx@finc.com\` コマンドを実行し、ユーザー登録を行ってからご利用下さい。`;
   }
 
   getConflictErrorMessage(): string {
     return `前回の測定が続いたままになっています。
-実装開始時間の打刻をしたい場合は、一度 \`/kaihatsu stop\` コマンドを実行し、前回の測定を停止して下さい。
-もし計測データを修正したい場合は \`/kaihatsu help\` コマンドからドキュメントをご参照下さい。
+実装開始時間の打刻をしたい場合は、一度 \`/measurement stop\` コマンドを実行し、前回の測定を停止して下さい。
+もし計測データを修正したい場合は \`/measurement help\` コマンドからドキュメントをご参照下さい。
 `;
   }
 }
--- src/use_case/measurement/stop/measurement_stop_output_data.ts
+++ src/use_case/measurement/stop/measurement_stop_output_data.ts
@@ -8,12 +8,12 @@ export class MeasurementStopOutputData {
 
   getUserNotFoundErrorMessage(userName: string): string {
     return `${userName} さんのユーザーデータは存在しません。
-\`/kaihatsu create_user xxx@finc.com\` コマンドを実行し、ユーザー登録を行ってからご利用下さい。`;
+\`/measurement user xxx@finc.com\` コマンドを実行し、ユーザー登録を行ってからご利用下さい。`;
   }
 
   getFirstTimeMeasurementMessage(userName: string): string {
     return `${userName} さんはこれが初めての計測のようです。
-まずは \`/kaihatsu start\` コマンドを実行し、計測を開始しましょう。`;
+まずは \`/measurement start\` コマンドを実行し、計測を開始しましょう。`;
   }
 
   getMeasurementNotStartedErrorMessage(): string {
