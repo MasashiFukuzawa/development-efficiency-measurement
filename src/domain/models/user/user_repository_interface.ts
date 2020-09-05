d7e3ba6fa752f331a5c1ba9bf26fcc1eb52efc44
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-09-05 22:31:56 +0900

    fix: interface attribute info

--- src/domain/models/user/user_repository_interface.ts
+++ src/domain/models/user/user_repository_interface.ts
@@ -4,7 +4,7 @@ export interface UserRepositoryInterface {
   getAll(): readonly User[];
   getRawData(): readonly any[][];
   map(data: any[][]): readonly User[];
-  putCache(...attributes: any): void;
+  putCache(userId: string, userName: string, now: Date): void;
   findByUserId(userId: string): User | null;
   create(userId: string, userName: string): User;
 }
